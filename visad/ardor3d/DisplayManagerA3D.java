72b4f45ec43e5ef615d7fd9e243988fc94edb775
rink <rink@moonunit.ssec.wisc.edu>
2018-01-22 15:46:47 -0600

    Remove use of MouseControlA3D. Replace with mouse events captured from Ardor3D forwarded to visad MouseHelper via application created MouseEvents (not the actual AWT events, this are consumed by Ardor3D).
    Supply a custom Camera to the canvasRenderer. Eventually add hooks to configure.
    
    Resize the camera on componentResized events. TODO:  maintain original aspect ratio when user changes window size.

--- visad/ardor3d/DisplayManagerA3D.java
+++ visad/ardor3d/DisplayManagerA3D.java
@@ -1,5 +1,6 @@
 package visad.ardor3d;
 
+import com.ardor3d.framework.Canvas;
 import com.ardor3d.framework.CanvasRenderer;
 import com.ardor3d.framework.DisplaySettings;
 import com.ardor3d.framework.FrameHandler;
@@ -7,20 +8,40 @@ import com.ardor3d.framework.Updater;
 import com.ardor3d.framework.jogl.JoglCanvasRenderer;
 import com.ardor3d.framework.jogl.awt.JoglAwtCanvas;
 import com.ardor3d.framework.jogl.awt.JoglSwingCanvas;
+import com.ardor3d.input.Key;
+import com.ardor3d.input.KeyboardState;
+import com.ardor3d.input.MouseButton;
+import com.ardor3d.input.MouseState;
 import com.ardor3d.input.PhysicalLayer;
 import com.ardor3d.input.awt.AwtFocusWrapper;
 import com.ardor3d.input.awt.AwtKeyboardWrapper;
 import com.ardor3d.input.awt.AwtMouseManager;
 import com.ardor3d.input.awt.AwtMouseWrapper;
 import com.ardor3d.input.logical.DummyControllerWrapper;
+import com.ardor3d.input.logical.InputTrigger;
 import com.ardor3d.input.logical.LogicalLayer;
+import com.ardor3d.input.logical.MouseButtonPressedCondition;
+import com.ardor3d.input.logical.MouseButtonReleasedCondition;
+import com.ardor3d.input.logical.MouseMovedCondition;
+import com.ardor3d.input.logical.TriggerAction;
+import com.ardor3d.input.logical.TriggerConditions;
+import com.ardor3d.input.logical.TwoInputStates;
+import com.ardor3d.math.Ray3;
+import com.ardor3d.math.Vector2;
+import com.ardor3d.math.Vector3;
+import com.ardor3d.renderer.Camera;
 import com.ardor3d.scenegraph.Node;
 import com.ardor3d.util.ReadOnlyTimer;
 import com.ardor3d.util.Timer;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.jogamp.newt.event.InputEvent;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.SwingUtilities;
 
 
@@ -64,13 +85,23 @@ public class DisplayManagerA3D implements Updater {
         
         root = dspRenderer.getRoot();
         transform = dspRenderer.getTransformNode();
-        
-        
-        mouseControl = new MouseControlA3D(transform);
-        mouseControl.setupMouseTriggers(logicalLayer);
-        
-        
+ 
         canvasRenderer = new JoglCanvasRenderer(dspRenderer);
+        
+        // Custom settings for Camera
+        Camera camera = new Camera(size.width, size.height);
+        camera.setFrustumPerspective(60f, size.width/size.height, 1, 100);
+        final Vector3 loc = new Vector3(0.0f, 0.0f, 5f);
+        final Vector3 left = new Vector3(-1.0f, 0.0f, 0.0f);
+        final Vector3 up = new Vector3(0.0f, 1.0f, 0.0f);
+        final Vector3 dir = new Vector3(0.0f, 0f, -1.0f);
+        /** Move our camera to a correct place and orientation. */
+        camera.setFrame(loc, left, up, dir);
+        canvasRenderer.setCamera(camera);
+        
+        dspRenderer.setCanvasRenderer(canvasRenderer);
+        ((MouseBehaviorA3D)dspRenderer.getMouseBehavior()).setCanvasRenderer(canvasRenderer);
+        
         
         final DisplaySettings settings = new DisplaySettings(size.width, size.height, 24, 0, 0, 16, 0, 0, false, false);        
         
@@ -84,13 +115,146 @@ public class DisplayManagerA3D implements Updater {
             public void componentResized(ComponentEvent e) {
                 super.componentResized(e);
                 if (size != null && !canvas.getSize().equals(size)) {
+                   canvasRenderer.getCamera().resize(canvas.getWidth(), canvas.getHeight());
                    markNeedDraw();
                 }
             }
         });
         
+        
+        
+        // handle mouse event conversion to MouseHelper here
+        
+        final TriggerAction leftPressedAction = new TriggerAction() {
+            @Override
+            public void perform(final Canvas source, final TwoInputStates inputStates, final double tpf) {
+                final MouseState mouse = inputStates.getCurrent().getMouseState();
+                
+                final KeyboardState keyboard = inputStates.getCurrent().getKeyboardState();
+                forwardToMouseHelper(MouseEvent.MOUSE_PRESSED, java.awt.event.InputEvent.BUTTON1_MASK, mouse, keyboard);
+            }
+        };
+        logicalLayer.registerTrigger(new InputTrigger(new MouseButtonPressedCondition(MouseButton.LEFT), leftPressedAction));
+        
+        final TriggerAction cntrPressedAction = new TriggerAction() {
+            @Override
+            public void perform(final Canvas source, final TwoInputStates inputStates, final double tpf) {
+                final MouseState mouse = inputStates.getCurrent().getMouseState();
+                final KeyboardState keyboard = inputStates.getCurrent().getKeyboardState();
+                forwardToMouseHelper(MouseEvent.MOUSE_PRESSED, java.awt.event.InputEvent.BUTTON2_MASK, mouse, keyboard);                
+            }
+        };        
+        logicalLayer.registerTrigger(new InputTrigger(new MouseButtonPressedCondition(MouseButton.MIDDLE), cntrPressedAction));
+        
+        final TriggerAction rightPressedAction = new TriggerAction() {
+            @Override
+            public void perform(final Canvas source, final TwoInputStates inputStates, final double tpf) {
+                final MouseState mouse = inputStates.getCurrent().getMouseState();
+                final KeyboardState keyboard = inputStates.getCurrent().getKeyboardState();
+                forwardToMouseHelper(MouseEvent.MOUSE_PRESSED, java.awt.event.InputEvent.BUTTON3_MASK, mouse, keyboard);
+
+            }
+        };        
+        logicalLayer.registerTrigger(new InputTrigger(new MouseButtonPressedCondition(MouseButton.RIGHT), rightPressedAction));
+        
+        final TriggerAction leftReleasedAction = new TriggerAction() {
+            @Override
+            public void perform(final Canvas source, final TwoInputStates inputStates, final double tpf) {
+                final MouseState mouse = inputStates.getCurrent().getMouseState();
+                final KeyboardState keyboard = inputStates.getCurrent().getKeyboardState();
+                forwardToMouseHelper(MouseEvent.MOUSE_RELEASED, java.awt.event.InputEvent.BUTTON1_MASK, mouse, keyboard);
+
+            }
+        };
+        logicalLayer.registerTrigger(new InputTrigger(new MouseButtonReleasedCondition(MouseButton.LEFT), leftReleasedAction));
+        
+        final TriggerAction cntrReleasedAction = new TriggerAction() {
+            @Override
+            public void perform(final Canvas source, final TwoInputStates inputStates, final double tpf) {
+                final MouseState mouse = inputStates.getCurrent().getMouseState();
+                final KeyboardState keyboard = inputStates.getCurrent().getKeyboardState();
+                forwardToMouseHelper(MouseEvent.MOUSE_RELEASED, java.awt.event.InputEvent.BUTTON2_MASK, mouse, keyboard);
+            }
+        };        
+        logicalLayer.registerTrigger(new InputTrigger(new MouseButtonReleasedCondition(MouseButton.MIDDLE), cntrReleasedAction));
+        
+        final TriggerAction rightReleasedAction = new TriggerAction() {
+            @Override
+            public void perform(final Canvas source, final TwoInputStates inputStates, final double tpf) {
+                final MouseState mouse = inputStates.getCurrent().getMouseState();
+                final KeyboardState keyboard = inputStates.getCurrent().getKeyboardState();
+                forwardToMouseHelper(MouseEvent.MOUSE_RELEASED, java.awt.event.InputEvent.BUTTON3_MASK, mouse, keyboard);
+            }
+        };        
+        logicalLayer.registerTrigger(new InputTrigger(new MouseButtonReleasedCondition(MouseButton.RIGHT), rightReleasedAction));        
+        
+        final Predicate<TwoInputStates> leftDownMouseMoved = Predicates.and(TriggerConditions.leftButtonDown(), TriggerConditions.mouseMoved());
+        final Predicate<TwoInputStates> cntrDownMouseMoved = Predicates.and(TriggerConditions.middleButtonDown(), TriggerConditions.mouseMoved());
+        final Predicate<TwoInputStates> rghtDownMouseMoved = Predicates.and(TriggerConditions.rightButtonDown(), TriggerConditions.mouseMoved());        
+
+        final TriggerAction leftDraggedAction = new TriggerAction() {
+            @Override
+            public void perform(final Canvas source, final TwoInputStates inputStates, final double tpf) {
+                final MouseState mouse = inputStates.getCurrent().getMouseState();
+                final KeyboardState keyboard = inputStates.getCurrent().getKeyboardState();
+                forwardToMouseHelper(MouseEvent.MOUSE_DRAGGED, java.awt.event.InputEvent.BUTTON1_MASK, mouse, keyboard);
+            }
+        };
+        logicalLayer.registerTrigger(new InputTrigger(leftDownMouseMoved, leftDraggedAction));
+        
+        final TriggerAction cntrDraggedAction = new TriggerAction() {
+            @Override
+            public void perform(final Canvas source, final TwoInputStates inputStates, final double tpf) {
+                final MouseState mouse = inputStates.getCurrent().getMouseState();
+                final KeyboardState keyboard = inputStates.getCurrent().getKeyboardState();
+                forwardToMouseHelper(MouseEvent.MOUSE_DRAGGED, java.awt.event.InputEvent.BUTTON2_MASK, mouse, keyboard);
+            }
+        };        
+        logicalLayer.registerTrigger(new InputTrigger(cntrDownMouseMoved, cntrDraggedAction));
+        
+        final TriggerAction rightDraggedAction = new TriggerAction() {
+            @Override
+            public void perform(final Canvas source, final TwoInputStates inputStates, final double tpf) {
+                final MouseState mouse = inputStates.getCurrent().getMouseState();
+                final KeyboardState keyboard = inputStates.getCurrent().getKeyboardState();
+                forwardToMouseHelper(MouseEvent.MOUSE_DRAGGED, java.awt.event.InputEvent.BUTTON3_MASK, mouse, keyboard);
+            }
+        };        
+        logicalLayer.registerTrigger(new InputTrigger(rghtDownMouseMoved, rightDraggedAction));
+        
+        final TriggerAction mouseMovedAction = new TriggerAction() {
+            @Override
+            public void perform(final Canvas source, final TwoInputStates inputStates, final double tpf) {
+                final MouseState mouse = inputStates.getCurrent().getMouseState();
+                final KeyboardState keyboard = inputStates.getCurrent().getKeyboardState();
+                forwardToMouseHelper(MouseEvent.MOUSE_MOVED, 0, mouse, keyboard);
+            }           
+        };
+        // Best only if component of canvas has focus? So many events otherwise.
+        //logicalLayer.registerTrigger(new InputTrigger(new MouseMovedCondition(), mouseMovedAction));
+        
+        
+        
         frameWork.addUpdater(this);
         start();
+    }
+    
+    private void forwardToMouseHelper(int id, int button, MouseState mState, KeyboardState kbState) {
+       int mod = 0;
+       
+       mod |= button;
+       
+       if (kbState.isDown(Key.LSHIFT) || kbState.isDown(Key.RSHIFT)) {
+          mod |= InputEvent.SHIFT_MASK;
+       }       
+       if (kbState.isDown(Key.LCONTROL) || kbState.isDown(Key.RCONTROL)) {
+          mod |= InputEvent.CTRL_MASK;
+       }
+       
+       Dimension size = canvas.getSize();
+       
+       MouseEvent me = new MouseEvent(canvas, id, 0, mod, mState.getX(), (size.height-mState.getY()), 0, false);
+       dspRenderer.getMouseBehavior().getMouseHelper().processEvent(me);
     }
     
     public Component getCanvas() {
