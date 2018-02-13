81953d83fcaa1519aa648081bba24d330df41b61
rink <rink@moonunit.ssec.wisc.edu>
2018-02-13 15:17:12 -0600

    Don't check for RenderContext in runner loop for now.

--- visad/ardor3d/RunnerA3D.java
+++ visad/ardor3d/RunnerA3D.java
@@ -1,36 +1,21 @@
 package visad.ardor3d;
 
-import com.ardor3d.framework.CanvasRenderer;
 import com.ardor3d.framework.FrameHandler;
-import com.ardor3d.framework.Updater;
-import com.ardor3d.renderer.ContextCapabilities;
-import com.ardor3d.renderer.RenderContext;
 
 public class RunnerA3D implements Runnable {
    private static final long FrameUpdateIntervalMillis = 20;
    
    private final FrameHandler frameWork;
-   private final CanvasRenderer canvasRenderer;
-   private final DisplayRendererA3D dspRenderer;
-   private final Updater updater;
    private Thread thread;
    private boolean pause = false;
    private boolean exit = false;
    
-   public RunnerA3D(FrameHandler frameWork, CanvasRenderer canvasRenderer, DisplayRendererA3D dspRenderer, Updater updater) {
+   public RunnerA3D(FrameHandler frameWork) {
       this.frameWork = frameWork;
-      this.canvasRenderer = canvasRenderer;
-      this.dspRenderer = dspRenderer;
-      this.updater = updater;
    }
    
    public void run() {
         frameWork.init();
-        /* This must be done after init above */
-        RenderContext renderContext = canvasRenderer.getRenderContext();
-        ContextCapabilities contextCapabilities = renderContext.getCapabilities();
-        dspRenderer.setCapabilities(contextCapabilities);
-        
         while (!exit) {
            if (!pause) {
               /* experiment
