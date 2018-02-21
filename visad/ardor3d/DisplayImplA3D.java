54141023cfda93c216b1e780b9880332db52d1d7
rink <rink@moonunit.ssec.wisc.edu>
2018-02-21 12:00:29 -0600

    make NPOT=true the default for A3D

--- visad/ardor3d/DisplayImplA3D.java
+++ visad/ardor3d/DisplayImplA3D.java
@@ -161,7 +161,7 @@ public class DisplayImplA3D extends DisplayImpl {
   // can enable this based on the Canvas3D properties.
   public static final boolean TEXTURE_NPOT;
   static {
-    TEXTURE_NPOT = Boolean.parseBoolean(System.getProperty(PROP_TEXTURE_NPOT, "false"));
+    TEXTURE_NPOT = Boolean.parseBoolean(System.getProperty(PROP_TEXTURE_NPOT, "true"));
     //System.err.println("TEXTURE_NPOT:"+TEXTURE_NPOT);
   }
   
@@ -1068,90 +1068,90 @@ public class DisplayImplA3D extends DisplayImpl {
 
       /* Test61: Volume Rendering */
      
-//    RealType xr = RealType.getRealType("xr");
-//    RealType yr = RealType.getRealType("yr");
-//    RealType zr = RealType.getRealType("zr");
-//    RealType wr = RealType.getRealType("wr");
-//    RealType[] types3d = {xr, yr, zr};
-//    RealTupleType earth_location3d = new RealTupleType(types3d);
-//    FunctionType grid_tuple = new FunctionType(earth_location3d, wr);
-//
-//    int NX = 256;
-//    int NY = 256;
-//    int NZ = 256;
-//    Integer3DSet set = new Integer3DSet(NX, NY, NZ);
-//    FlatField grid3d = new FlatField(grid_tuple, set);
-//
-//    float[][] values = new float[1][NX * NY * NZ];
-//    int k = 0;
-//    for (int iz=0; iz<NZ; iz++) {
-//      // double z = Math.PI * (-1.0 + 2.0 * iz / (NZ - 1.0));
-//      double z = Math.PI * (-1.0 + 2.0 * iz * iz / ((NZ - 1.0)*(NZ - 1.0)) );
-//      for (int iy=0; iy<NY; iy++) {
-//        double y = -1.0 + 2.0 * iy / (NY - 1.0);
-//        for (int ix=0; ix<NX; ix++) {
-//          double x = -1.0 + 2.0 * ix / (NX - 1.0);
-//          double r = x - 0.5 * Math.cos(z);
-//          double s = y - 0.5 * Math.sin(z);
-//          double dist = Math.sqrt(r * r + s * s);
-//          values[0][k] = (float) ((dist < 0.1) ? 10.0 : 1.0 / dist);
-//          k++;
-//        }
-//      }
-//    }
-//    grid3d.setSamples(values);
-//
-//    display.addMap(new ScalarMap(xr, Display.XAxis));
-//    display.addMap(new ScalarMap(yr, Display.YAxis));
-//    display.addMap(new ScalarMap(zr, Display.ZAxis));
-//
-//    ScalarMap xrange = new ScalarMap(xr, Display.SelectRange);
-//    ScalarMap yrange = new ScalarMap(yr, Display.SelectRange);
-//    ScalarMap zrange = new ScalarMap(zr, Display.SelectRange);
-//    display.addMap(xrange);
-//    display.addMap(yrange);
-//    display.addMap(zrange);
-//
-//    GraphicsModeControl mode = display.getGraphicsModeControl();
-//    mode.setScaleEnable(true);
-//
-//    mode.setTexture3DMode(GraphicsModeControl.STACK2D);
-//
-//    // new
-//    RealType duh = RealType.getRealType("duh");
-//    int NT = 32;
-//    Linear2DSet set2 = new Linear2DSet(0.0, (double) NX, NT,
-//                                       0.0, (double) NY, NT);
-//    RealType[] types2d = {xr, yr};
-//    RealTupleType domain2 = new RealTupleType(types2d);
-//    FunctionType ftype2 = new FunctionType(domain2, duh);
-//    float[][] v2 = new float[1][NT * NT];
-//    for (int i=0; i<NT*NT; i++) {
-//      v2[0][i] = (i * i) % (NT/2 +3);
-//    }
-//    // float[][] v2 = {{1.0f,2.0f,3.0f,4.0f}};
-//    FlatField field2 = new FlatField(ftype2,set2);
-//    field2.setSamples(v2);
-//    display.addMap(new ScalarMap(duh, Display.RGB));
-//
-//    ScalarMap map1color = new ScalarMap(wr, Display.RGBA);
-//    display.addMap(map1color);
-//
-//    ColorAlphaControl control = (ColorAlphaControl) map1color.getControl();
-//    control.setTable(buildTable(control.getTable()));
-//
-//    DataReferenceImpl ref_grid3d = new DataReferenceImpl("ref_grid3d");
-//    ref_grid3d.setData(grid3d);
-//
-//    DataReferenceImpl ref2 = new DataReferenceImpl("ref2");
-//    ref2.setData(field2);
-//
-//    ConstantMap[] cmaps = {new ConstantMap(0.0, Display.TextureEnable)};
-//    display.addReference(ref2, cmaps);
-//
-//    display.addReference(ref_grid3d, null);
-//
-//    widget = getSpecialComponent(display);
+    RealType xr = RealType.getRealType("xr");
+    RealType yr = RealType.getRealType("yr");
+    RealType zr = RealType.getRealType("zr");
+    RealType wr = RealType.getRealType("wr");
+    RealType[] types3d = {xr, yr, zr};
+    RealTupleType earth_location3d = new RealTupleType(types3d);
+    FunctionType grid_tuple = new FunctionType(earth_location3d, wr);
+
+    int NX = 100;
+    int NY = 100;
+    int NZ = 100;
+    Integer3DSet set = new Integer3DSet(NX, NY, NZ);
+    FlatField grid3d = new FlatField(grid_tuple, set);
+
+    float[][] values = new float[1][NX * NY * NZ];
+    int k = 0;
+    for (int iz=0; iz<NZ; iz++) {
+      // double z = Math.PI * (-1.0 + 2.0 * iz / (NZ - 1.0));
+      double z = Math.PI * (-1.0 + 2.0 * iz * iz / ((NZ - 1.0)*(NZ - 1.0)) );
+      for (int iy=0; iy<NY; iy++) {
+        double y = -1.0 + 2.0 * iy / (NY - 1.0);
+        for (int ix=0; ix<NX; ix++) {
+          double x = -1.0 + 2.0 * ix / (NX - 1.0);
+          double r = x - 0.5 * Math.cos(z);
+          double s = y - 0.5 * Math.sin(z);
+          double dist = Math.sqrt(r * r + s * s);
+          values[0][k] = (float) ((dist < 0.1) ? 10.0 : 1.0 / dist);
+          k++;
+        }
+      }
+    }
+    grid3d.setSamples(values);
+
+    display.addMap(new ScalarMap(xr, Display.XAxis));
+    display.addMap(new ScalarMap(yr, Display.YAxis));
+    display.addMap(new ScalarMap(zr, Display.ZAxis));
+
+    ScalarMap xrange = new ScalarMap(xr, Display.SelectRange);
+    ScalarMap yrange = new ScalarMap(yr, Display.SelectRange);
+    ScalarMap zrange = new ScalarMap(zr, Display.SelectRange);
+    display.addMap(xrange);
+    display.addMap(yrange);
+    display.addMap(zrange);
+
+    GraphicsModeControl mode = display.getGraphicsModeControl();
+    mode.setScaleEnable(true);
+
+    mode.setTexture3DMode(GraphicsModeControl.STACK2D);
+
+    // new
+    RealType duh = RealType.getRealType("duh");
+    int NT = 32;
+    Linear2DSet set2 = new Linear2DSet(0.0, (double) NX, NT,
+                                       0.0, (double) NY, NT);
+    RealType[] types2d = {xr, yr};
+    RealTupleType domain2 = new RealTupleType(types2d);
+    FunctionType ftype2 = new FunctionType(domain2, duh);
+    float[][] v2 = new float[1][NT * NT];
+    for (int i=0; i<NT*NT; i++) {
+      v2[0][i] = (i * i) % (NT/2 +3);
+    }
+    // float[][] v2 = {{1.0f,2.0f,3.0f,4.0f}};
+    FlatField field2 = new FlatField(ftype2,set2);
+    field2.setSamples(v2);
+    display.addMap(new ScalarMap(duh, Display.RGB));
+
+    ScalarMap map1color = new ScalarMap(wr, Display.RGBA);
+    display.addMap(map1color);
+
+    ColorAlphaControl control = (ColorAlphaControl) map1color.getControl();
+    control.setTable(buildTable(control.getTable()));
+
+    DataReferenceImpl ref_grid3d = new DataReferenceImpl("ref_grid3d");
+    ref_grid3d.setData(grid3d);
+
+    DataReferenceImpl ref2 = new DataReferenceImpl("ref2");
+    ref2.setData(field2);
+
+    ConstantMap[] cmaps = {new ConstantMap(0.0, Display.TextureEnable)};
+    display.addReference(ref2, cmaps);
+
+    display.addReference(ref_grid3d, null);
+
+    widget = getSpecialComponent(display);
 
 
       /* If we have a control widget */
