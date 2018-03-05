b57913d94ef1ecf46c4c0cb6ed0f3b1177cb9f08
rink <rink@moonunit.ssec.wisc.edu>
2018-03-05 14:42:42 -0600

    reduce this class to VisAD independent

--- visad/ardor3d/SceneA3D.java
+++ visad/ardor3d/SceneA3D.java
@@ -1,99 +1,338 @@
 package visad.ardor3d;
 
+import com.ardor3d.framework.CanvasRenderer;
 import com.ardor3d.framework.Scene;
 import com.ardor3d.intersection.PickResults;
-import com.ardor3d.light.PointLight;
+import com.ardor3d.intersection.PickingUtil;
+import com.ardor3d.intersection.PrimitivePickResults;
 import com.ardor3d.math.ColorRGBA;
 import com.ardor3d.math.Ray3;
-import com.ardor3d.math.Vector3;
+import com.ardor3d.math.Transform;
+import com.ardor3d.math.type.ReadOnlyColorRGBA;
+import com.ardor3d.renderer.ContextCapabilities;
 import com.ardor3d.renderer.Renderer;
-import com.ardor3d.renderer.queue.RenderBucketType;
-import com.ardor3d.renderer.state.LightState;
-import com.ardor3d.renderer.state.ZBufferState;
+import com.ardor3d.renderer.state.ClipState;
+import com.ardor3d.renderer.state.RenderState;
 import com.ardor3d.scenegraph.Node;
-import com.ardor3d.scenegraph.event.DirtyType;
+import com.ardor3d.scenegraph.Spatial;
+import visad.ardor3d.SwitchNode;
+import java.util.Vector;
+import java.util.concurrent.Callable;
 
-public class SceneA3D implements Scene {
-   
-   private final Node root;
-   private final Node transform;
-   
-   private boolean needDraw = false;
+
+public abstract class SceneA3D implements Scene
+{
+
+  // for screen locked
+  private OrderedNode screen_locked = null;
+  private TransformNode locked_trans = null;
+
+  /** root Node of scene graph for this display */
+  private com.ardor3d.scenegraph.Node root = null;
+  
+  /** single Node for a common model Transfrom between root and Nodes holding
+   * all Data depiction sub-graphs */
+  private TransformNode trans = null;
+  
+  private OrderedNode non_direct = null;
+
+
+  /** MouseBehaviorA3D */
+  private MouseBehaviorA3D mouse = null;
+
+  /** KeyboardBehaviorA3D */
+  private KeyboardBehaviorA3D keyboard = null;
+
+  /** background attached to root */
+  private Object background = null;
+
+  /** TransformGroup between trans and cursor */
+  private TransformNode cursor_trans = null;
+  
+  /** single Switch between cursor_trans and cursor */
+  private SwitchNode cursor_switch = null;
+  
+  /** children of cursor_switch */
+  private Node cursor_on = null, cursor_off = null;
+  
+  /** on / off state of cursor */
+  private boolean cursorOn = false;
+  
+  /** on / off state of direct manipulation location display */
+  private boolean directOn = false;
+
+  /** single Switch between trans and box */
+  private SwitchNode box_switch = null;
+  
+  /** children of box_switch */
+  private Node box_on = null, box_off = null;
+  
+  /** on / off state of box */
+  private boolean boxOn = false;
+
+  /** single Switch between trans and scales */
+  private SwitchNode scale_switch = null;
+  
+  /** children of scale_switch */
+  private Node scale_on = null, scale_off = null;
+  
+  /** Vector of screen based AxisScales */
+  private Vector axis_vector = new Vector();
+
+  /** on / off state of cursor in GraphicsModeControl */
+
+  /** cursor location */
+  private float cursorX, cursorY, cursorZ;
+  
+  /** normalized direction perpendicular to current cursor plane */
+  private float line_x, line_y, line_z;
+  
+  /** start value for cursor */
+  private float point_x, point_y, point_z;
+  
+  private boolean needDraw = false;
+  
+  private final Object MUTEX = new Object();
+
+  /** ModelClip stuff, done by reflection */
+  private boolean[] modelClipEnables =
+    {false, false, false, false, false, false};
+  
+  private final ClipState rootClipState;
+  
+  private ContextCapabilities contextCapabilities;
+  
+  private CanvasRenderer canvasRenderer;
+  
+  private DisplayManagerA3D dspManager;
+  
+  ReadOnlyColorRGBA boxColor = ColorRGBA.WHITE;
+  ReadOnlyColorRGBA cursorColor = ColorRGBA.WHITE;
+  
+  
+  public SceneA3D () {
+    super();
+      this.rootClipState = (ClipState) RenderState.createState(RenderState.StateType.Clip);
+  }
+
+
+  public Node getRoot() {
+    return root;
+  }
+
+  public TransformNode getTransformNode() {
+     return trans;
+  }
+
+  public Node getCursorOnBranch() {
+    return cursor_on;
+  }
+
+  public Node getBoxOnBranch() {
+    return box_on;
+  }
+
+  public abstract Node createSceneGraph();
+
+  public Node createBasicSceneGraph(MouseBehaviorA3D m) {
+    if (root != null) return root;
+
+    mouse = m;
+
+    // Create the root of the branch graph
+    root = new Node();
     
-   public SceneA3D() {
-      root = new Node();
-      transform = new Node();
-      
-      /**
-       * Create a ZBuffer to display pixels closest to the camera above
-       * farther ones.
-       */
-      final ZBufferState buf = new ZBufferState();
-      buf.setEnabled(true);
-      buf.setFunction(ZBufferState.TestFunction.LessThanOrEqualTo);
-      root.setRenderState(buf);
+    locked_trans = new TransformNode();
+    screen_locked = new OrderedNode();
+    locked_trans.attachChild(screen_locked);
+    Node node = new Node();
+    node.attachChild(locked_trans);
+    root.attachChild(node);
+    
+    
+    // create the TransformNode that is the parent of Data object Group objects
+    setTransform(null);
+    root.attachChild(trans);
 
-      // ---- LIGHTS
-      /**
-       * Set up a basic, default light.
-       */
-      PointLight light = new PointLight();
-      PointLight light2 = new PointLight();
-      light2.setLocation(new Vector3(-100,-100,-100));
-      light2.setEnabled(true);
+    // create background
+    /*
+    background = new Background();
+    float[] ctlBg = getRendererControl().getBackgroundColor();
+    background.setColor(ctlBg[0], ctlBg[1], ctlBg[2]);
+    BoundingSphere bound2 = new BoundingSphere(new Point3d(0.0,0.0,0.0),2000000.0);
+    background.setApplicationBounds(bound2);
+    root.addChild(background);
+    */
 
-      light.setDiffuse(new ColorRGBA(0.5f, 0.5f, 0.5f, 1.0f));
-      light.setAmbient(new ColorRGBA(0.05f, 0.05f, 0.05f, 1.0f));
-      light.setLocation(new Vector3(100, 100, 100));
-      light.setEnabled(true);
+    // WLH 10 March 2000
+    non_direct = new OrderedNode();
+    trans.attachChild(non_direct);
 
-      /**
-       * Attach the light to a lightState and the lightState to rootNode.
-       */
-      LightState lightState = new LightState();
-      lightState.setEnabled(true);
-      lightState.setTwoSidedLighting(false);
-      lightState.attach(light);
-      lightState.attach(light2);
-      root.setRenderState(lightState);
+    cursor_trans = new TransformNode();
+    trans.attachChild(cursor_trans);
+    cursor_switch = new SwitchNode();
+    cursor_trans.attachChild(cursor_switch);
+    cursor_on = new Node();
+    cursor_off = new Node();
+    cursor_switch.attachChild(cursor_off);
+    cursor_switch.attachChild(cursor_on);
+    cursor_switch.setSingleVisible(0); // initially off
+    cursorOn = false;
 
-      root.getSceneHints().setRenderBucketType(RenderBucketType.Opaque);
+    box_switch = new SwitchNode();
+    trans.attachChild(box_switch);
+    box_on = new Node();
+    box_off = new Node();
+    box_switch.attachChild(box_off);
+    box_switch.attachChild(box_on);
+    box_switch.setSingleVisible(1); // initially on
 
-      root.attachChild(transform);      
-   }
+    scale_switch = new SwitchNode();
+    trans.attachChild(scale_switch);
+    scale_on = new Node();
+    scale_off = new Node();
+    scale_switch.attachChild(scale_off);
+    scale_switch.attachChild(scale_on);
+    scale_switch.setSingleVisible(0); // initially off
+
+    root.setRenderState(rootClipState);
+
+    return root;
+  }  
+
+  // WLH 23 Oct 2001
+  /** 
+   * Define a clipping plane in (XAxis, YAxis, ZAxis) space.  Allows
+   * up to 6 arbitrary planes.  Each clip plane is defined by the equation:
+   * <PRE>
+   *      aX + bY + cZ + d <= 0
+   * </PRE>
+   * <p>Example useage:</p>
+   * To clip to the usual VisAD cube (i.e., x, y and z values in the 
+   * range -1.0 to +1.0) (see Test35.java), call:
+   * <PRE>
+   *    DisplayRendererJ3D dr = 
+   *             (DisplayRendererJ3D) display.getDisplayRenderer();
+   *    dr.setClip(0, true,  1.0f,  0.0f,  0.0f, -1.01f);  // X_POS face
+   *    dr.setClip(1, true, -1.0f,  0.0f,  0.0f, -1.01f);  // X_NEG face
+   *    dr.setClip(2, true,  0.0f,  1.0f,  0.0f, -1.01f);  // Y_POS face
+   *    dr.setClip(3, true,  0.0f, -1.0f,  0.0f, -1.01f);  // Y_NEG face
+   *    dr.setClip(4, true,  0.0f,  0.0f,  1.0f, -1.01f);  // Z_POS face
+   *    dr.setClip(5, true,  0.0f,  0.0f, -1.0f, -1.01f);  // Z_NEG face
+   * </PRE>
+   * <b>Note:</b> d value is slightly less than -1.0 so items in the plane
+   *              are not clipped.
+   * @param  plane  plane number must be in (0, ..., 5)).
+   * @param  enable true to enable clipping on this plane, false to disable
+   * @param  a      x coefficent
+   * @param  b      y coefficent
+   * @param  c      z coefficent
+   * @param  d      constant
+   * @throws  VisADException  illegal plane argument or 
+   *                          unsupported (&lt; 1.2) version of Java 3D
+   */
+  public void setClip(int plane, boolean enable, float a, float b, float c, float d) {
+    rootClipState.setClipPlaneEquation(plane, a, b, c, d);
+    rootClipState.setEnabled(enable);
+  }
+
+  public void addSceneGraphComponent(Object group) {
+       Callable updateCallable = new Callable() {
+          public Object call() {
+             canvasRenderer.makeCurrentContext();
+             non_direct.attachChild((Node)group);
+             canvasRenderer.releaseCurrentContext();
+             return null;
+          }
+       };
+       //GameTaskQueue uQueue = DisplayManagerA3D.queueManager.getQueue(GameTaskQueue.RENDER);
+       //uQueue.enqueue(updateCallable);
+       //uQueue.execute();    
+    non_direct.attachChild((Node)group);
+  }
+
+  public void addLockedSceneGraphComponent(Object node) {
+    if (screen_locked == null) return;
+    screen_locked.attachChild((Node)node);
+  }
+
+  public void updateLockedTrans(double[] matrix) {
+    if (locked_trans != null) {
+      //locked_trans.setTransform(new Transform3D(matrix));
+    }
+  }
+
+  public void clearScene(Object group) {
+    if (group != null) {
+      non_direct.detachChild((Spatial)group);
+    }
+  }
+
+  public void setTransform(Transform t) {
+    if (trans == null) {
+      trans = new TransformNode();
+    }
+    if (t != null) {
+      Callable updateCallable = new Callable() {
+          public Object call() {
+             canvasRenderer.makeCurrentContext();
+             trans.setTransform(t);
+             canvasRenderer.releaseCurrentContext();
+             return null;
+          }
+      };
+      //GameTaskQueue uQueue = DisplayManagerA3D.queueManager.getQueue(GameTaskQueue.RENDER);
+      //uQueue.enqueue(updateCallable);
+      //uQueue.execute();
+      trans.setTransform(t);
+    }
+  }
+
+  public void setLineWidth(float width) {
+  }
+
+  public int getTextureWidthMax() {
+//    return contextCapabilities.getMaxTextureSize();
+      return 8192;
+  }
+
+  public int getTextureHeightMax() {
+//    return contextCapabilities.getMaxTextureSize();
+      return 8192;
+  }
+  
+  public void setCapabilities(ContextCapabilities obj) {
+     this.contextCapabilities = obj;
+  }
 
    @Override
    public boolean renderUnto(Renderer renderer) {
-       if (transform.isDirty(DirtyType.Transform)) {
-          root.updateGeometricState(20, true);
-          root.draw(renderer);
-          return true;
-       }
-       else if (needDraw) {
-          synchronized(this) {
-             root.updateGeometricState(20, true);
-             root.draw(renderer);
-             needDraw = false;
-          }
-          return true;
-       }
-       return false;      
+      root.onDraw(renderer);
+      return true;
    }
-
+      
    @Override
    public PickResults doPick(Ray3 pickRay) {
+      final PickResults pickResults = new PrimitivePickResults();
+      pickResults.setCheckDistance(true);
+      PickingUtil.findPick(getRoot(), pickRay, pickResults);
       return null;
    }
    
-   public synchronized void markNeedDraw() {
-      needDraw = true;
+   void setCanvasRenderer(CanvasRenderer canvasRenderer) {
+      this.canvasRenderer = canvasRenderer;
    }
    
-   public Node getRootNode() {
-      return root;
+   public CanvasRenderer getCanvasRenderer() {
+      return canvasRenderer;
+   }
+
+   void setDisplayManager(DisplayManagerA3D aThis) {
+      this.dspManager = aThis;
    }
    
-   public Node getTransformNode() {
-      return transform;
+   public DisplayManagerA3D getDisplayManager() {
+      return this.dspManager;
    }
-}
+
+ }
